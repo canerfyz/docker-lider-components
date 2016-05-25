0487c25a05f1dd2abbcf6749fac8e3ff126da38e
canerfyz <caner.feyzullahoglu@agem.com.tr>
2016-05-26 00:54:19 +0300

    dpkg-reconfigure and Starting script for OpenLDAP

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+if [ ! -e /etc/ldap/bootstrap.lock ]; then
+  echo "Configuring OpenLDAP"
+  cat <<EOF | debconf-set-selections
+slapd slapd/internal/generated_adminpw password ${LDAP_ROOTPASS:-secret}
+slapd slapd/internal/adminpw password ${LDAP_ROOTPASS:-secret}
+slapd slapd/password2 password ${LDAP_ROOTPASS:-secret}
+slapd slapd/password1 password ${LDAP_ROOTPASS:-secret}
+slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
+slapd slapd/domain string ${LDAP_DOMAIN:-agem.com.tr}
+slapd shared/organization string ${LDAP_ORGANISATION:-AGEM Bilisim}
+slapd slapd/backend string MDB
+slapd slapd/purge_database boolean true
+slapd slapd/move_old_database boolean true
+slapd slapd/allow_ldap_v2 boolean false
+slapd slapd/no_configuration boolean false
+slapd slapd/dump_database select when needed
+EOF
+  DEBIAN_FRONTEND=noninteractive dpkg-reconfigure -f noninteractive slapd
+
+  echo "Bootstrap finished."
+  touch /etc/ldap/bootstrap.lock
+else
+  echo "Already bootstrapped. Skipping."
+fi
+
+echo "Starting OpenLDAP"
+exec nohup slapd -h "ldap:/// ldapi:///" -u openldap -g openldap ${LDAP_OPTS:-} -d ${LDAP_DEBUG:-"stats"} &
+
